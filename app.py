e363d97e234b4f9a9c2444e54cfc273870c622d1
Mangalore <mangalos@3c22fb7b33df.ant.amazon.com>
2021-02-24 01:20:17 -0600

    Updated to read config from AppConfig

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+import os
+
+from config_helper import AppConfigHelper
+
 from flask import Flask
+import boto3
+from botocore.exceptions import ClientError
+
 app = Flask(__name__)
+
+appconfig = AppConfigHelper(
+    os.environ['ConfigApp'],
+    os.environ['ConfigEnv'],
+    os.environ['ConfigProfile'],
+    3600,
+    os.environ['ConfigClient']
+)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    ddb_client = boto3.client('dynamodb',
+                              region_name=os.environ['AWS_DEFAULT_REGION'])
+    TABLE_NAME = get_table_name()
+    try:
+        response = ddb_client.get_item(
+            TableName=TABLE_NAME, Key={'Application': {'S': 'TwelveFactorApp'}})
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return f"Hello from {response['Item']['Name']['S']}"
+
+
+def get_table_name():
+    appconfig.update_config()
+    return appconfig.config["TableName"]
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-
-
+    app.run(host='0.0.0.0', port=8000)
